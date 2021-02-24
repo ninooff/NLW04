abfc7c3d7b5b60baf6033de646c9405738ccb37e
Antonino Branco <79278621+ninooff@users.noreply.github.com>
2021-02-24 13:57:13 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ExperienceBar } from './components/ExperienceBar';
+import './styles/global.css';
+
+//gerador de html
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
